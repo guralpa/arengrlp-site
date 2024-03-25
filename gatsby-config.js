392fae93db86b0b07e09d0fe1a9188b15068472d
Aren Guralp <142130640+guralpa@users.noreply.github.com>
2024-03-25 19:24:20 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ArenSite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}

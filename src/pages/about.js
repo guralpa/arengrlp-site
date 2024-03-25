e9935edf8289c4003b48a6bfb38f887b5259e79d
Aren Guralp <142130640+guralpa@users.noreply.github.com>
2024-03-25 19:50:27 -0400

    Add about.js

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (<main>
+    <h1>About Me</h1>
+    <p>Hello! I am a mathematics student at Brown University.</p>
+    </main>)
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
